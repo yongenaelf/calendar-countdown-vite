bcdb600485866232daec187f7cf2623684d5bf17
yongenaelf <132553186+yongenaelf@users.noreply.github.com>
2026-01-09 14:51:32 +0800

    fix: routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,7 +11,7 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/calendar-countdown-vite">
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
         <Route path="/holidays" element={<HolidayListScreen />} />
